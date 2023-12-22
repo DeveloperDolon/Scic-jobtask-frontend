a440e2fb39f47c0cd58867696cd7c3dbb3af0584
Dolon Roy <dolonr718@gmail.com>
2023-12-22 12:25:43 +0600

    form validation with react form hook commit

--- src/pages/SignPage.jsx
+++ src/pages/SignPage.jsx
@@ -81,7 +81,7 @@ const SignPage = () => {
                     </div>
                     <div className="mt-8">
                         <button role="button" aria-label="create my account" className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full">
-                            Create my account
+                            Login my account
                         </button>
                     </div>
                 </div>
